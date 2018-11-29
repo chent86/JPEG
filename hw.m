6e4c8863a60fe2a2da7495935df1eb76e3bdd61d
Chent <bz20100405@163.com>
2018-11-30 07:17:33 +0800

    add save jpg part

--- hw.m
+++ hw.m
@@ -1,45 +1,46 @@
-img=imread('C:/Users/lenovo/Desktop/hw/2.jpg');
-imfinfo('C:/Users/lenovo/Desktop/hw/2.jpg')
+% img=imread('C:/Users/lenovo/Desktop/hw/2.jpg');
+% imfinfo('C:/Users/lenovo/Desktop/hw/2.jpg')
+% 
+% [height, width, depth] = size(img);
+% 
+% JPEG压缩部分
+% img = RGB_to_YIQ(img);      % RGB转为YIQ
+% 
+% img = subsample(img);       % 4:2:0二次采样
+% 
+% img = DCT(img);             % DCT变换
+% 
+% img = quantization(img);    %量化  
+% 
+% AC = RLC(img);              %AC系数的游长编码
+% 
+% DC = DPCM(img);             %DC系数的DPCM编码
 
-[height, width, depth] = size(img);
+% huffman_DC = huffman_for_DC(DC);       %DC系数的huffman编码
+% 
+% huffman_AC = huffman_for_AC(AC);       %AC系数的huffman编码
 
-%JPEG压缩部分
-img = RGB_to_YIQ(img);      % RGB转为YIQ
+save_jpg(huffman_AC, huffman_DC); 
 
-img = subsample(img);       % 4:2:0二次采样
-
-img = DCT(img);             % DCT变换
-
-img = quantization(img);    %量化  
-
-AC = RLC(img);              %AC系数的游长编码
-
-DC = DPCM(img);             %DC系数的DPCM编码
-
-huffman_DC = huffman_for_DC(DC);       %DC系数的huffman编码
-
-huffman_AC = huffman_for_AC(AC);       %AC系数的huffman编码
-
-
-%JPEG解压缩部分
-new_img = zeros(height, width, depth);
-
-new_DC = inv_huffman_for_DC(huffman_DC);   %DC系数的huffman解码
-
-new_AC = inv_huffman_for_AC(huffman_AC);     %AC系数的huffman解码
-
-
-new_img = inv_DPCM(new_img, new_DC);    %DC系数的游长编码解码
-
-new_img = inv_RLC(new_img, new_AC);     %AC系数的DPCM解码
-
-new_img = un_quantization(new_img);     %反量化
-
-new_img = IDCT(new_img);                %逆DCT变换
-
-new_img = YIQ_to_RGB(new_img);          %YIQ转为RGB
-
-figure,imshow(new_img);
+% %JPEG解压缩部分
+% new_img = zeros(height, width, depth);
+% 
+% new_DC = inv_huffman_for_DC(huffman_DC);   %DC系数的huffman解码
+% 
+% new_AC = inv_huffman_for_AC(huffman_AC);     %AC系数的huffman解码
+% 
+% 
+% new_img = inv_DPCM(new_img, new_DC);    %DC系数的游长编码解码
+% 
+% new_img = inv_RLC(new_img, new_AC);     %AC系数的DPCM解码
+% 
+% new_img = un_quantization(new_img);     %反量化
+% 
+% new_img = IDCT(new_img);                %逆DCT变换
+% 
+% new_img = YIQ_to_RGB(new_img);          %YIQ转为RGB
+% 
+% figure,imshow(new_img);
 
 
 
---
+++ save_jpg.m
@@ -0,0 +1,56 @@
+function [] = save_jpg(huffman_AC, huffman_DC)
+%将JPEG压缩后的数据保存到文件
+    fid = fopen('C:/Users/lenovo/Desktop/hw/1.txt','a');
+    binary='';
+    for d =1:3
+        [~,width] = size(huffman_AC(d).compress_data);
+        for j=1:width
+           binary = [binary, num2str(huffman_AC(d).compress_data(j))];
+        end
+        [~,width] = size(huffman_DC(d).compress_data);
+        for j=1:width
+           binary = [binary, num2str(huffman_DC(d).compress_data(j))];
+        end
+        [height,~] = size(huffman_AC(d).dict);
+        for j=1:height
+           m = cell2mat(huffman_AC(d).dict(j,2));
+           [~,width] = size(m);
+           for k=1:width
+               binary = [binary, num2str(m(k))];
+           end
+        end
+        [height,~] = size(huffman_DC(d).dict);
+        for j=1:height
+           m = cell2mat(huffman_DC(d).dict(j,2));
+           [~,width] = size(m);
+           for k=1:width
+               binary = [binary, num2str(m(k))];
+           end
+        end
+        [~,width] = size(huffman_DC(d).binary);
+        for j=1:width
+           if  huffman_DC(d).binary(j)=='-'
+              huffman_DC(d).binary(j)='0'; 
+           end
+        end
+        [~,width] = size(huffman_AC(d).binary);
+        for j=1:width
+           if  huffman_AC(d).binary(j)=='-'
+              huffman_AC(d).binary(j)='0'; 
+           end
+        end
+        binary = [binary, huffman_DC(d).binary, huffman_AC(d).binary];
+    end
+    result = '';
+    [~,width] = size(binary);
+    add = 4-mod(width,4);
+    for i=1:add
+       binary = [binary,'0']; 
+    end
+    for i=1:4:width
+        result = [result, dec2hex(bin2dec(binary(i:i+3)))];
+    end
+    fprintf(fid,'%s',result);
+    fclose(fid);
+end
+
